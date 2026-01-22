5ec496c17b495ae4459db34309f11a35c1d483e5
Lucas Oliveira <76497363+LucasRamosOliveira@users.noreply.github.com>
2026-01-22 11:33:21 +0000

    fix tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -2,9 +2,9 @@ import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices()ç
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-})
+});
 
 test("POST to /api/v1/migrations shoud return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,8 +1,8 @@
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices()ç
-})
+  await orchestrator.waitForAllServices();
+});
 
 test("GET to /api/v1/status shoud return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
